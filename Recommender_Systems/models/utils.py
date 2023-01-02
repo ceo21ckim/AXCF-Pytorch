009d510ea65b98fd7a00ef31a1956c3f4ce686ae
Dongeon Kim <60685175+ceo21ckim@users.noreply.github.com>
2023-01-02 21:18:33 +0900

    Delete Recommender_Systems directory

--- Recommender_Systems/models/utils.py
+++
@@ -1,9 +0,0 @@
-import torch 
-
-def calc_accuracy(pred_y, true_y):
-    pred_y = torch.sigmoid(pred_y)
-    return ((pred_y > 0.5) == true_y).sum().detach().cpu().item()
-
-def sentiment_score(x):
-    if x >= 3.5 : return 1
-    elif x < 3.5 : return 0
